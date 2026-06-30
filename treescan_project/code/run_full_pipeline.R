5d1a1ffb773c4541c9c71d7da2fb8b622f8230d8
NyallJamieson <nyalljamieson2000@outlook.com>
2026-06-30 11:15:13 -0500

    update line change

--- treescan_project/code/run_full_pipeline.R
+++ treescan_project/code/run_full_pipeline.R
@@ -129,7 +129,7 @@ if (lag_choice == 1){
     lines <- readLines(script_path)
     
     # Replace target line
-    lines[136] <- new_line
+    lines[140] <- new_line
     
     # Save script
     writeLines(lines, script_path)
